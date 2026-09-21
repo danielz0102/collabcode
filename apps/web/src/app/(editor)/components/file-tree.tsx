6ca8a364c5e7a93edac08797961bf923a4f07909
danielz0102 <danyzglez@hotmail.com>
2026-09-21 17:14:24 -0600

    feat(web): add icons for folders and files

--- apps/web/src/app/(editor)/components/file-tree.tsx
+++ apps/web/src/app/(editor)/components/file-tree.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { cn } from "cn"
+import { Folder, File, ChevronDown, ChevronRight } from "lucide-react"
 import { useState, type PropsWithChildren } from "react"
 
 type FileNode = {
@@ -58,15 +59,23 @@ function TreeItem({ node, paddingLeft = 0 }: { node: TreeNode; paddingLeft?: num
 }
 
 function FileItem({ node, paddingLeft = 0 }: { node: FileNode; paddingLeft?: number }) {
-  return <TreeItemButton paddingLeft={paddingLeft}>{node.name}</TreeItemButton>
+  return (
+    <TreeItemButton paddingLeft={paddingLeft}>
+      <File size={16} />
+      {node.name}
+    </TreeItemButton>
+  )
 }
 
 function FolderItem({ node, paddingLeft = 0 }: { node: FolderNode; paddingLeft?: number }) {
   const [isOpen, setIsOpen] = useState(false)
+  const Arrow = isOpen ? ChevronDown : ChevronRight
 
   return (
     <>
       <TreeItemButton paddingLeft={paddingLeft} onClick={() => setIsOpen((prev) => !prev)}>
+        <Arrow size={16} />
+        <Folder size={16} />
         {node.name}
       </TreeItemButton>
 
@@ -86,7 +95,7 @@ type TreeItemButtonProps = PropsWithChildren<{
 function TreeItemButton({ paddingLeft = 0, onClick, children }: TreeItemButtonProps) {
   return (
     <button
-      className="w-full cursor-pointer p-1 text-left text-nowrap select-none hover:bg-neutral-600"
+      className="flex w-full cursor-pointer items-center gap-1 p-1 text-left text-nowrap select-none hover:bg-neutral-600"
       style={{ paddingLeft: paddingLeft || 4 }}
       onClick={onClick}
     >
